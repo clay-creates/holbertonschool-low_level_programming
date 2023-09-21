d101787dcdc2d5348560c742d2feb5e33ebb8966
Clay Jones <claycreatesstuff@gmail.com>
2023-09-21 14:48:06 -0700

    change

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
-/*
-*
-* main - entry point to script / program
-*
-* Returns 0 on demand
-*
-*/
-
+/**
+ *
+ * main - entry point to script / program
+ *
+ * Returns 0 on demand
+ *
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\\n");
 return (0);
 }
