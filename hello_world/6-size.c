d4db96db388ba8d30947d851c5846ade079ec308
Clay Jones <claycreatesstuff@gmail.com>
2023-09-21 15:12:25 -0700

    make low lvl script 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: returns 0 on request
+ */
+int main(null)
+{
+printf("Size of a char: 1 byte(s)\n", sizeof(char));
+printf("Size of an int: 4 byte(s)\n", sizeof(int));
+printf("Size of a long int: 8 byte(s)\n", sizeof(long));
+printf("Size of a long long int: 8 byte(s)\n", sizeof(long long));
+printf("Size of a float: 4 byte(s)\n", sizeof (float));
+return (0);;
+}
