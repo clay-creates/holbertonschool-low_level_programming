9493ff0d61854f7bb86c32fae2a7498a44589562
Clay Jones <claycreatesstuff@gmail.com>
2023-09-21 12:40:44 -0700

    more

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
+/*Program printing out words using puts function*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
