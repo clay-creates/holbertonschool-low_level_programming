cc3d3e6a84704118fb6c4f81d14ebfa40a766eb0
Clay Jones <claycreatesstuff@gmail.com>
2023-09-22 11:41:54 -0700

    make var script 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0 on request
+ */
+
+int main(void)
+{
+	char num;
+
+	num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
