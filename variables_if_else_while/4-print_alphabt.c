626c1a9b9c5fca524dc71630d155c578dee36847
Clay Jones <claycreatesstuff@gmail.com>
2023-09-22 11:26:35 -0700

    make var script 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0 on request
+ */
+
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+	if (letter != 'e')
+	(letter != 'q');
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	putchar('\n');
+		return (0);
+}
