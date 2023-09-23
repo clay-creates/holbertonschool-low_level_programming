d238d2ffbee32d7e28b3e94c8e5cbaf517f31ee8
Clay Jones <claycreatesstuff@gmail.com>
2023-09-23 10:33:04 -0700

    make nested script 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: returns 0 on request
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int target;
+
+	letter = 'a';
+	target = 0;
+
+	while (target < 10)
+	{
+		while (letter <= 'z')
+		{
+			putchar(letter);
+			letter++;
+			if (letter == 'z')
+				letter = 'a';
+		}
+		target++;
+		putchar('\n');
+	}
+}
+
