a2553cf5c8d79051b2d48f9202e68d40a5bc64f5
Clay Jones <claycreatesstuff@gmail.com>
2023-09-25 16:03:00 -0700

    move thing

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,11 +14,9 @@ void jack_bauer(void)
 	hours = 0;
 	mins = 0;
 
-	while (hours < 24);
-		(hours++);
+	while (hours < 24)
 	{
-		while (mins < 60);
-			(mins++);
+		while (mins < 60)
 		{
 			_putchar('0' + (hours / 10));
 			_putchar('0' + (hours % 10));
@@ -26,6 +24,8 @@ void jack_bauer(void)
 			_putchar('0' + (mins / 10));
 			_putchar('0' + (mins % 10 ));
 			_putchar('\n');
+			mins++;
 		}
+		hours++;
 	}
 }
