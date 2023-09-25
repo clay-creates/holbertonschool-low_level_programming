a2a66ec5b12f11a04ca1c81ce488b7457216685d
Clay Jones <claycreatesstuff@gmail.com>
2023-09-25 11:21:45 -0700

    test

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,17 +12,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf(const char *'+');
-		return (1);
+		putchar('+'); || return (1);
 	}
 	else if (n == 0)
 	{
-		printf(const char *'0');
-		return (0);
+		putchar('0'); || return (0);
 	}
 	else
 	{
-		printf(const char *'-');
-		return (-1);
+		putchar('-'); || return (-1);
 	}
 }
