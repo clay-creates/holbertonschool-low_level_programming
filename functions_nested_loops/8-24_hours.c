0da1e2311de7375e2c4595f9c992fcf6e45b3a34
Clay Jones <claycreatesstuff@gmail.com>
2023-09-25 16:05:01 -0700

    if statement

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -22,9 +22,13 @@ void jack_bauer(void)
 			_putchar('0' + (hours % 10));
 			_putchar(':');
 			_putchar('0' + (mins / 10));
-			_putchar('0' + (mins % 10 ));
+			_putchar('0' + (mins % 10));
 			_putchar('\n');
 			mins++;
+		}
+		if (mins > 60)
+		{
+			mins = 0;
 		}
 		hours++;
 	}
