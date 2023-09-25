5e4bf9eabecfcbaa196aac647a12f1832f97e4f6
Clay Jones <claycreatesstuff@gmail.com>
2023-09-25 16:35:47 -0700

    thing

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -20,16 +20,27 @@ void times_table(void)
 		while (j <= 9)
 		{
 			sum = i * j;
-			if (sum < 10)
+
+			if (sum == 0)
 			{
-				_putchar('0' + sum);
+				_putchar('0');
 			}
 			else
 			{
-				_putchar('0' + (sum / 10));
-				_putchar('0' + (sum % 10));
+				_putchar(',');
+				_putchar(' ');
 			}
-			if (j < 9)
+			       
+				if (sum < 10)
+				{
+					_putchar('0' + sum);
+				}
+				else
+				{
+					_putchar('0' + (sum / 10));
+					_putchar('0' + (sum % 10));
+				}
+			if (j < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
