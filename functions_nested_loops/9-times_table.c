eec89d1c825cd56c9b5af26882e8631357356374
Clay Jones <claycreatesstuff@gmail.com>
2023-09-25 16:51:52 -0700

    another while

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,21 +11,34 @@ void times_table(void)
 	int i;
 	int j;
 	int sum;
+	int target;
 
 	i = 0;
 	j = 0;
+	target = 0;
 
-	while (i <= 9)
+	while (target < 9)
 	{
-		while (j <= 9)
+		while (i <= 9)
 		{
-			sum = i * j;
-			_putchar('0' + sum);
-			_putchar(',');
-			_putchar(' ');
-			j++;
-		}
+			while (j <= 9)
+			{
+				sum = i * j;
+				_putchar('0' + sum);
+				_putchar(',');
+				_putchar(' ');
+				j++;
+				if (j > 9)
+				{
+					j = 0;
+				}
+			}
 		i++;
+		if (i > 9)
+		{
+			i = 0;
+		}
+		}
+		target++;
 	}
-}
 
