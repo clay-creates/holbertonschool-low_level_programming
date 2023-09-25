f1b7fa933e83f5670b4fafb0e224247d62ff0a6a
Clay Jones <claycreatesstuff@gmail.com>
2023-09-25 16:26:46 -0700

    make nest script 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: returns 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int sum;
+
+	i = 0;
+	j = 0;
+
+	while (i <= 9)
+	{
+		while (j <= 9)
+		{
+			sum = i * j;
+			if (sum < 10)
+			{
+				_putchar('0' + sum);
+			}
+			else
+			{
+				_putchar('0' + (sum / 10));
+				_putchar('0' + (sum % 10));
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
