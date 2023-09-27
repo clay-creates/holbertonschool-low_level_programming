7e357c7abc46ee86d07636ab7faff5894ac21cad
Clay Jones <claycreatesstuff@gmail.com>
2023-09-27 11:32:42 -0700

    make more nest script 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints out a '\' n number of times
+ * @n: input passed to print_diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+	else if (n < 0)
+	{
+		_putchar('\n');
+	}
+}
