60c79e6bc0acdf254891a8801cd7973de80ddd8e
Clay Jones <claycreatesstuff@gmail.com>
2023-09-27 07:37:33 -0700

    make more nest script 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numers 0 to 9 followed by newline
+ *
+ * Return: returns 0 on success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 9)
+	{
+		if (i <= 9)
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
