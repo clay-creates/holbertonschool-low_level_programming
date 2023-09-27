00de39cb27e0882bb1e0ba9ebea2bf9d2df7b87a
Clay Jones <claycreatesstuff@gmail.com>
2023-09-27 10:55:40 -0700

    make more nest script 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - drawa a straight line in terminal
+ * @n: input passed to print_line, amount of lines
+ */
+
+void print_line(int n)
+{
+	_putchar('_' * n);
+	_putchar('\n');
+
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+}
