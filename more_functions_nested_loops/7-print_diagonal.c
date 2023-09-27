fa9cb5a058a712dec0f7d16067e6a965b8d22dcc
Clay Jones <claycreatesstuff@gmail.com>
2023-09-27 12:44:07 -0700

    thing

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,11 +11,16 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
+		int j;
+
 		for (i = 0; i < n; i++)
 		{
+			for (j =0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
-			_putchar(' ');
 		}
 	}
 	else
