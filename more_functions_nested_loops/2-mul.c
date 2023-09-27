e5a366e115acaadefde7c41de2e5942751ce7cef
Clay Jones <claycreatesstuff@gmail.com>
2023-09-27 07:24:52 -0700

    make more nest script 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first integer to multiply
+ * @b: second integer to multiply
+ *
+ * Return: returns 0 on success
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	_putchar(sum + '0');
+	return (0);
+}
