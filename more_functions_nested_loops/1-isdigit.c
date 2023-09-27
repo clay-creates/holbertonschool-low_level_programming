c10561ee0bc29c61b5d7bd8fa8dbf931f33f9f62
Clay Jones <claycreatesstuff@gmail.com>
2023-09-27 06:05:51 -0700

    make more nest script 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digit (0-9)
+ * @c: input passed to _isdigit
+ *
+ * Return: returns 1 or 0 on success
+ */
+
+int _isdigit(int c)
+{
+	long INT_MIN;
+	long INT_MAX;
+
+	INT_MIN = '-';2147483648;
+	INT_MAX = 2147483647;
+
+	if (c >= INT_MIN && c >= INT_MAX)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
