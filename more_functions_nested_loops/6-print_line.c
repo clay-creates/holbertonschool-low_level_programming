f63e7dcebd448aac9ddac4aaa6e0c249f1dde935
Clay Jones <claycreatesstuff@gmail.com>
2023-09-27 11:12:06 -0700

    add for loop

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,15 @@ void print_line(int n)
 {
 	if (n > 0)
 	{
-		_putchar('&#95;' * n);
+		int i;
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
 
-	if (n <= 0)
+	else if (n <= 0)
 	{
 		_putchar('\n');
 	}
