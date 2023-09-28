c646742d3af5a58c6cacea16b9e94a28abe28a74
Clay Jones <claycreatesstuff@gmail.com>
2023-09-28 07:21:41 -0700

    make more nest script 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a newline
+ * @size: input passed to print_triangle
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			_putchar(' ');
+		}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
