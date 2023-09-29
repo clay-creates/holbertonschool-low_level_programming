2d1964d17c37ca56cac26c57e65bd0c9acec4c84
Clay Jones <claycreatesstuff@gmail.com>
2023-09-29 09:14:44 -0700

    make pointer script 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length is calculated
+ */
+
+int _strlen(char *s)
+{
+	for char in s
+	{
+		_putchar('*s');
+	}
+}
