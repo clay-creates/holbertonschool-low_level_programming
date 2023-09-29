444075cc8908b148c6018a98ce43b07067f553aa
Clay Jones <claycreatesstuff@gmail.com>
2023-09-29 14:06:25 -0700

    try different

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,10 @@
 
 void print_rev(char *s)
 {
-	int length = strlen(s);
-	int i, j;
-
-	for (i = 0; i < j; i++)
+	while (*s != '\0')
 	{
-		j = length - 1;
-		j--;
-		char temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
+		putchar(*s - 1);
+		s--;
 	}
-	reversed[j] = '\0';
-	printf("%s\n", reversed);
+	putchar('\n');
 }
