8f697add21a5aff9815a966e105eaefabae8f78d
Clay Jones <claycreatesstuff@gmail.com>
2023-09-29 14:29:25 -0700

    remake

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,28 @@
 
 void print_rev(char *s)
 {
+	int i, j;
+	char *p1, *p2, temp;
 
-	while (*s != '\0')
+	//Get input length
+	i = strlen(*s)
+
+	//Set beginning pointer to start of string
+	p1 = *s;
+
+	//Set end pointer to end of string
+	p2 = *s + i - 1;
+
+	//Swap char from begin to end
+	for (j = 0; j < (i - 1) / 2; j++)
 	{
-		putchar(-*s);
-		
+		//Swap character
+		temp = *p2;
+		*p2 = *p1;
+		*p1 = temp;
+
+		p1++;
+		p2--;
 	}
 	putchar('\n');
 }
