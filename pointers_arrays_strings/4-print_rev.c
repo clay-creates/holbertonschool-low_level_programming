5591249c43222c367094481ca8af05fb813df785
Clay Jones <claycreatesstuff@gmail.com>
2023-09-29 14:36:42 -0700

    fix dat shit

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,28 +8,18 @@
 
 void print_rev(char *s)
 {
-	int i, j;
-	char *p1, *p2, temp;
+	int i = 0;
 
-	//Get input length
-	i = strlen(*s)
-
-	//Set beginning pointer to start of string
-	p1 = *s;
-
-	//Set end pointer to end of string
-	p2 = *s + i - 1;
-
-	//Swap char from begin to end
-	for (j = 0; j < (i - 1) / 2; j++)
+	while (s[i] != 0)
 	{
-		//Swap character
-		temp = *p2;
-		*p2 = *p1;
-		*p1 = temp;
-
-		p1++;
-		p2--;
+		i++;
 	}
-	putchar('\n');
+	i = i -1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
 }
