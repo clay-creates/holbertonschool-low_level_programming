9a6a845e9cd6f76b25a5c19ce822d10ab547d8b9
Clay Jones <claycreatesstuff@gmail.com>
2023-09-30 13:41:40 -0700

    ?????

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str[i + 1] != '\0')
 	{
 		_putchar(str[i]);
+		_putchar(str[i + 1]);
 		i += 2;
 	}
-	if (i > 0 && str[i - 2] != '\0')
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
