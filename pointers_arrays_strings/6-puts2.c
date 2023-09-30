29c70b892caeb42676a794da11cda2b64402e564
Clay Jones <claycreatesstuff@gmail.com>
2023-09-30 13:33:23 -0700

    add i var and +=

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,12 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		_putchar(*str);
-		str = str + 2;
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
