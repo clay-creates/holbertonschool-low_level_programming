6424741958ccd213d342da457329f35a21c9d9ae
Clay Jones <claycreatesstuff@gmail.com>
2023-10-01 10:40:51 -0700

    test

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-
 	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d, ", a[i]);
 	}
-	printf("%d\n", a[n - 1]);
+	if (n > 0)
+	{
+		printf("%d\n", a[n - 1]);
+	}
+	else
+	{
+		printf('\n');
 }
