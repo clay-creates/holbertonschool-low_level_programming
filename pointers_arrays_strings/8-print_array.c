eb8b4666120f13e4923747b5a68af46da4ebbef9
Clay Jones <claycreatesstuff@gmail.com>
2023-10-01 10:32:21 -0700

    make pointer script 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers, followed by newline
+ * @a: input of integer array
+ * @n: number of elements from array to be printed
+ */
+
+void print_array(int *a, int *n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i < *n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[*n - 1]);
+}
