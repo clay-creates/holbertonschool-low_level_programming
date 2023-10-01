35276fd54ffbd1d42bebffddc75660dfcbc42eea
Clay Jones <claycreatesstuff@gmail.com>
2023-10-01 10:35:13 -0700

    rm n pointer

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -7,18 +7,18 @@
  * @n: number of elements from array to be printed
  */
 
-void print_array(int *a, int *n)
+void print_array(int *a, int n)
 {
 	int i;
 
-	if (*n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 
-	for (i = 0; i < *n - 1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d, ", a[i]);
 	}
-	printf("%d\n", a[*n - 1]);
+	printf("%d\n", a[n - 1]);
 }
