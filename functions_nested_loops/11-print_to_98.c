be330db42adca30a672166a07195be8b1f85269e
Clay Jones <claycreatesstuff@gmail.com>
2023-10-02 14:03:02 -0700

    make nest script 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: integer to start from
+ */
+
+void print_to_98(int n)
+{
+	int i = 98;
+
+	while (n < i)
+	{
+		printf("%d ,", n);
+		n++;
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+}	
