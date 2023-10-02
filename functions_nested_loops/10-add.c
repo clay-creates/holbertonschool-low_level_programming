fc12000a08f6ca9e3ebe62adccb8cd6957a8dce0
Clay Jones <claycreatesstuff@gmail.com>
2023-10-02 13:55:12 -0700

    did the thing

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,13 +2,15 @@
 
 /**
  * add - add two integers
+ * @i: integer one
+ * @j: integer two
  *
  * Return: return the result
  */
 
 int add(int i, int j)
 {
-	int result = i * j;
+	int result = i + j;
 
 	return (result);
 }
