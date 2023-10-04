ef4f42af13ae35f7e39a44587e125e45b1cac80e
Clay Jones <claycreatesstuff@gmail.com>
2023-10-04 13:32:19 -0700

    make more pointer script 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to be copied
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dest_start = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest++ = *src++;
+		n--;
+	}
+	return (dest_start);
+}
