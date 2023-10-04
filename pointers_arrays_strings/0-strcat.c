141a05950fa20dfe050102697b284053e8f4fffb
Clay Jones <claycreatesstuff@gmail.com>
2023-10-04 10:02:41 -0700

    fix increment

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,11 @@ char *_strcat(char *dest, char *src)
 {
 	while (*dest != '\0')
 	{
-		*dest++;
+		dest++;
 	}
 	while (*src != '\0')
 	{
-		dest = src;
-		*src++;
-		*dest++;
+		*dest++ = *src++;
 	}
 	*dest = '\0';
 	return (dest);
