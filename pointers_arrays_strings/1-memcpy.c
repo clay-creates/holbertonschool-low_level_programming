f494cf650b65c9df3bb6843540e1d2316414d6f1
Clay Jones <claycreatesstuff@gmail.com>
2023-10-06 10:31:46 -0700

    add **sptr and increment

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+	char **sptr = *src;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest++	= *src++;
+		*dest++	= *sptr++;
 	}
 	return (dest);
 }
