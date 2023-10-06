e4c9edc04cc2882cee3c810c2dcb3ff0421ff67e
Clay Jones <claycreatesstuff@gmail.com>
2023-10-06 10:16:45 -0700

    dereference

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,12 +12,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *dptr = dest;
 	char *sptr = src;
 
 	for (i = 0; i < n; i++)
 	{
-		dest = *sptr;
+		*dest = *sptr;
 		sptr++;
 	}
 	return (dest);
