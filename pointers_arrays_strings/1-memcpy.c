bfd392f0bf61d39248b246d1eb83fa33268ce1ad
Clay Jones <claycreatesstuff@gmail.com>
2023-10-06 10:33:54 -0700

    revert

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char **sptr = src;
+	char *sptr = src;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest++	= **sptr++;
+		*dest++	= *sptr++;
 	}
 	return (dest);
 }
