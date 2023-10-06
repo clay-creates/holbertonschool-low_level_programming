f97a7c92e7981fb2d3caace32c4fc84765fd8935
Clay Jones <claycreatesstuff@gmail.com>
2023-10-06 12:02:47 -0700

    add stddef

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,18 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
  * @s: string being searched
  * @c: character being searched for
  *
- * Return: returns a pointer to the first occurance of character, or null 
+ * Return: returns a pointer to the first occurance of character, or null
  */
 
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *cptr;
+	char *cptr = NULL;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
