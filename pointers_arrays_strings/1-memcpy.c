bbd2e10a83a9da25f1e3c16de907e1a7e35cee37
Clay Jones <claycreatesstuff@gmail.com>
2023-10-06 10:08:49 -0700

    make even more pointer script 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies the memory area
+ * @dest: memory area destination
+ * @src: source memory area to be copied
+ * @n: number of bytes to be copied
+ *
+ * Return: return pointer to destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *dptr = dest;
+	char *sptr = src;
+
+	for (i = 0; i < n; i++)
+	{
+		dest = *sptr;
+		sptr++;
+	}
+	return (dest);
+}
