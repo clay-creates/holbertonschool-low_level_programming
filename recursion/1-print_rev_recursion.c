c475d90dfb8c264e86f8fa04669575a503680bd9
Clay Jones <claycreatesstuff@gmail.com>
2023-10-11 15:12:08 -0700

    make recursion script 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be reversed
+ *
+ * Return: exit if statement
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+	}
+
+	if (*s < 0)
+	{
+		putchar('\n');
+		return;
+	}
+
+	putchar(*s);
+	_print_rev_recursion(s - 1);
+}
