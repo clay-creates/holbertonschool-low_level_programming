ac271515ec0f39bc3e86bfd5c4ecee234bafa613
Clay Jones <claycreatesstuff@gmail.com>
2023-10-12 07:51:26 -0700

    make recursion script 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be counted
+ * @count: count of characters
+ *
+ * Return: returns count once recursion is finished
+ */
+
+int _strlen_recursion(char *s, int count)
+{
+	if (*s == '\0')
+	{
+		return (count);
+	}
+	else
+	{
+		return (_strlen_recursion(s + 1, count + 1));
+	}
+}
