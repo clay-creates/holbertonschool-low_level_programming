8c63858245117fc1bb2b2ec9c552b679a8fea77b
Clay Jones <claycreatesstuff@gmail.com>
2023-10-12 08:30:14 -0700

    make recursion script 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number to find factorial of
+ *
+ * Return: returns the factorial number, or returns -1 if number less than 0
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	factorial(n - 1);
+}
