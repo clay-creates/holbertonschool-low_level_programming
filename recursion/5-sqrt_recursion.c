1dc4deaf3c83aace5b2367f31293c34972daa846
Clay Jones <claycreatesstuff@gmail.com>
2023-10-13 08:10:29 -0700

    make recursion script 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural sqaure root of a number
+ * @n: number to find square root of
+ *
+ * Return: returns integer of sqare root, or -1 if not
+ */
+
+int _sqrt_recurion(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * _sqrt_recursion(n / 2));
+	}
+}
