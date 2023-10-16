ada9a2c39c0d0b5d341a67f616f1d22b4dda2c7b
Clay Jones <claycreatesstuff@gmail.com>
2023-10-16 09:15:34 -0700

    remake with for and another if

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point, adds positive positive numbers
+ * @argc: argument count
+ * @argv: array of argument names
+ *
+ * Return: returns 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
+
+		if (num <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
