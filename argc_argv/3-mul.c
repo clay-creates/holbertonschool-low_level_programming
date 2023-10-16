dfb1259204cafebeead03a41013f2a5202ed7d56
Clay Jones <claycreatesstuff@gmail.com>
2023-10-16 06:52:50 -0700

    initialize result

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 {
 	int num1;
 	int num2;
-	int result = num1 * num2;
+	int result;
 
 	if (argc < 3)
 	{
@@ -23,6 +23,7 @@ int main(int argc, char **argv)
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
