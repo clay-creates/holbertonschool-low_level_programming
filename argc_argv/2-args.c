6682ff5b645c27df3c57a5ef16cf12bc5a6537c2
Clay Jones <claycreatesstuff@gmail.com>
2023-10-15 22:07:03 -0700

    mv if statement

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -12,10 +12,10 @@
 int main(int argc, char **argv)
 {
 	int i;
-
-	for (i = 0; i < argc; i++);
+	
+	if (argc >= 1)
 	{
-		if (argc >= 1)
+		for (i = 0; i < argc; i++)
 		{
 			printf("%s\n", *argv[i]);
 		}
