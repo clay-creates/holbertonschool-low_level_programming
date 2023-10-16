f8a484472e0701ea670597aa402b01cbea350442
Clay Jones <claycreatesstuff@gmail.com>
2023-10-15 22:00:22 -0700

    make arg program 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point, prints all arguments
+ * @argc: number of arguments
+ * @argv: array of argument names
+ *
+ * Return: return 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0, i < argc, i++)
+	{
+		printf("%d\n", argv[i]);
+	}
+	retun (0);
+}
