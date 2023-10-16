697e4cafde41e35a22c85172d17b123f5909e4ef
Clay Jones <claycreatesstuff@gmail.com>
2023-10-15 21:34:21 -0700

    documentation

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - entry point, prints name of program
+ * @argc: count of arguements
+ * @argv: array of arguement names
+ */
+
 int main(int argc, char **argv)
 {
 	if (argc >= 1)
