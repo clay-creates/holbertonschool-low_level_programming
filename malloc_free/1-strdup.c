78d25a6c899cfea5cfdb860234c890f48caaf1be
Clay Jones <claycreatesstuff@gmail.com>
2023-10-18 10:00:15 -0700

    make malloc script 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+#include <stddef.h>
+
+/**
+ * _strdup - returns a pointer to allocated memory holding a copy of a string
+ * @str: string to be copied
+ *
+ * Return: return pointer to string copy on success or NULL on fail
+ */
+
+char *_strdup(char *str)
+{
+	char *string;
+
+	string = (char *)malloc(size * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	return (string);
+	free (string);
+}
