6e25e713071700aeb69a1a4418500e8efc574c2c
Clay Jones <claycreatesstuff@gmail.com>
2023-10-19 11:12:22 -0700

    make malloc script 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - initializes grid with value of 0
+ * @width: width of grid
+ * @height: height of grid
+ *
+ * Return: returns pointer to 2D array or NULL on fail
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int *grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = calloc(height * width *sizeof(int));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	return (grid);
+}
