b9aa734ad6f87026da89b94a7c946f838a335b7a
Clay Jones <claycreatesstuff@gmail.com>
2023-10-19 15:06:51 -0700

    test

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < width; i++)
 	{
-		grid[i] = malloc(sizeof(int *) * height);
+		grid[i] = malloc(sizeof(int) * height);
 
 		if (grid[i] == NULL)
 		{
---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free space allocated by alloc_grid
+ * @grid: pointer pointing to grid
+ * @height: rows in grid
+ */
+
+void free_grid(int **grid, int height)
+{
+
