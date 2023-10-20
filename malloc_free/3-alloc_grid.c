06b8d3967186a35521748662a5665509e3f2f6c0
Clay Jones <claycreatesstuff@gmail.com>
2023-10-20 06:23:37 -0700

    initialie 0

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
 
@@ -39,6 +39,14 @@ int **alloc_grid(int width, int height)
 
 			free(grid);
 			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
 		}
 	}
 
