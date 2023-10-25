4891960ad882e572b4854851fda39a887313f052
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-25 21:01:13 +0000

    rm strcpy

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -18,7 +18,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		return;
 	}
 
-	strcpy(d->name, name);
+	d->name = name;
 	d->age = age;
-	strcpy(d->owner, owner);
+	d->owner = owner;
 }
