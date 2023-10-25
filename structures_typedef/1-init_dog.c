4b400fe5b865481ba45da158707eaceb7771e25b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-25 20:17:26 +0000

    null check

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,6 +12,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+
 	strcpy(d->name, name);
 	d->age = age;
 	strcpy(d->owner, owner);
