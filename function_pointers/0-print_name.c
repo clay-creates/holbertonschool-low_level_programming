1bdb3e5cbcb772cfed0ea074d4f7526debd5cec5
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-26 04:42:52 +0000

    tmp

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+}
