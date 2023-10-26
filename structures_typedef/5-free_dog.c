b109dccfbd59530ac7f015e3b2e849d9143f2e8d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-26 01:53:22 +0000

    free them dogs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -9,10 +9,16 @@
 
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
+    if (d != NULL)
     {
-        return (NULL);
+        if (d->name != NULL)
+        {
+            free(d->name);
+        }
+        if (d->owner != NULL)
+        {
+            free(d->owner);
+        }
+        free(d);
     }
-
-    free(d);
 }
