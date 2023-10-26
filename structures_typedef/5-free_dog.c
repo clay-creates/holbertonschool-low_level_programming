9501425e564797d538c56d5ff10f16158e92281d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-26 01:54:00 +0000

    indent

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -9,16 +9,16 @@
 
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        if (d->name != NULL)
-        {
-            free(d->name);
-        }
-        if (d->owner != NULL)
-        {
-            free(d->owner);
-        }
-        free(d);
-    }
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
 }
