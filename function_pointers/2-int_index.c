6a70f10f6a3bcc23c238d3269a97bc096afb43d6
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-26 20:07:23 +0000

    make pointer function 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,46 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int_index - function that searches for an integer
+ * @array: array to search
+ * @size: size of array
+ * @cmp: function to compare values
+ *
+ * Return: return index of first found integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL)
+	{
+		return;
+	}
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	if (cmp == NULL)
+	{
+		return;
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] == sizeof(int))
+			{
+				return (array[i]);
+			}
+			else
+			{
+				return (-1);
+			}
+		}
+	}
+}
