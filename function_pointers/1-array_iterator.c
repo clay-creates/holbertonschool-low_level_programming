e1b1f00536fa62ad1b2d26a08c472f50105640fc
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-26 19:14:25 +0000

    make pointer function 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,38 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array to iterate through
+ * @size: size of the array
+ * @action: pointer to function being used
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL)
+	{
+		return;
+	}
+
+	if (size < 0)
+	{
+		return;
+	}
+
+	if (action == NULL)
+	{
+		return;
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
