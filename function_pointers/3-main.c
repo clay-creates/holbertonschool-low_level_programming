40debbb280c2cc809bf75e93e146f4e557b98d97
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-27 17:17:45 +0000

    establish vars, if conditions, use get_op_func and returns

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,6 +10,33 @@ int (*get_op_func(char *s))(int, int);
  * Return: returns 0 on success, or 1 on fail
  */
 
-int main(void)
+int main(int argc, char **argv)
 {
+    int num1, num2, result;
+    char operator;
+
+    if (argc < 4)
+    {
+        return (1);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    operator= * argv[2];
+
+    if (operator== NULL)
+    {
+        return (2);
+    }
+
+    int (*calc_func)(int, int) = get_op_func(operator);
+
+    if (calc_func == NULL)
+    {
+        return (3);
+    }
+
+    result = calc_func(num1, num2);
+
+    return (result);
 }
