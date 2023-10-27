ac3969180cd8a00eb721ca9da34b1d92e0feef4a
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-27 04:48:12 +0000

    start main.c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int (*get_op_func(char *s))(int, int);
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0 on success, or 1 on fail
+ */
+
+int main(void)
+{
+}
