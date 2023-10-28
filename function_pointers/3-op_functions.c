1460cc72ed90bb84bdf56d9a93fa7c77bdf10eed
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-28 18:39:30 +0000

    update op functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -55,6 +55,10 @@ int op_div(int a, int b)
 	{
 		return (a / b);
 	}
+	else
+	{
+		return (NULL);
+	}
 }
 
 /**
@@ -70,5 +74,9 @@ int op_mod(int a, int b)
 	if (a != 0 || b != 0)
 	{
 		return (a % b);
+	}
+	else
+	{
+		return (NULL);
 	}
 }
