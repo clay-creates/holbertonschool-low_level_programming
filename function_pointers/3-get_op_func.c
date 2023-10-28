fdba3e61b78692f3fbc91886725e2676890da211
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-28 18:45:40 +0000

    parenteses test for get op function

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -20,11 +20,11 @@ int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
+		{'+', op_add},
+		{'-', op_sub},
+		{'*', op_mul},
+		{'/', op_div},
+		{'%', op_mod},
 		{NULL, NULL}};
 
 	while (ops[i].op != NULL)
