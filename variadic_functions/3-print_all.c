d279fee3722bf1bf3e95dd93656fd62846e54a49
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-30 17:30:33 +0000

    rm vars and edit loops

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,46 +13,38 @@ void print_all(const char *const format, ...)
     va_list args;
     int i = 0;
     char *str;
-    char f_i, f_end;
-
-    f_i = format[i];
-    f_end = format[i + 1];
 
     va_start(args, format);
 
-    while (format)
+    while (format && format[i])
     {
-        while (format[i] != '\0')
+        if (i > 0)
         {
-            switch (format[i])
-            {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (!str)
-                {
-                    printf("(nil)");
-                }
-                printf("%s", str);
-                break;
-            default:
-                break;
-            }
-            if (f_end != '\0' && (f_i == 'c' || f_i == 'i' || f_i == 'f' || f_i == 's'))
-            {
-                printf(", ");
-            }
-            i++;
+            printf(", ");
         }
-        break;
+        switch (format[i])
+        {
+        case 'c':
+            printf("%c", va_arg(args, int));
+            break;
+        case 'i':
+            printf("%d", va_arg(args, int));
+            break;
+        case 'f':
+            printf("%f", va_arg(args, double));
+            break;
+        case 's':
+            str = va_arg(args, char *);
+            if (!str)
+            {
+                printf("(nil)");
+            }
+            printf("%s", str);
+            break;
+        default:
+            break;
+        }
+        i++;
     }
 
     printf("\n");
