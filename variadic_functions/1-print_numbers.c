1114818635d0efca11c1b94582414fe1b77b44c2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-10-30 14:54:18 +0000

    make var program 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include "stdarg.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - function that prints numbers with newline
+ * @space: string to be printed between numbers
+ * @n: number of integers passed
+ */
+
+void print_numbers(const char *space, const unsigned int n, ...)
+{
+    unsigned int i;
+    va_list ap;
+
+    va_start(ap, n);
+
+    if (space == NULL)
+    {
+        space = "";
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", ap);
+    }
+    printf("\n");
+}
