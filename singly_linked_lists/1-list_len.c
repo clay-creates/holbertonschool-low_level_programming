c33e7208de70638f7501a90211033e4745fd7e22
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-08 16:52:29 +0000

    indentation

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -10,12 +10,12 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t counter = 0;
+	size_t counter = 0;
 
-    while (h != NULL)
-    {
-        counter++;
-        h = h->next;
-    }
-    return (counter);
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
 }
