74e1f69cd2f7a7438b7067384766962d78438cab
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-08 16:28:18 +0000

    add stdio and rm head from initial loop

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a list_t list
@@ -11,7 +12,7 @@ size_t print_list(const list_t *h)
 {
     size_t counter = 0;
 
-    while (h->head != NULL)
+    while (h != NULL)
     {
         if (h->str == NULL)
         {
