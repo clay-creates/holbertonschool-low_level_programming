ae647a45a7e5204594ed1207cdef6a8b68d8b025
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-09 19:24:30 +0000

    indentation

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,39 +13,39 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *current;
+	list_t *new, *current;
 
-    new = malloc(sizeof(list_t));
-    current = *head;
+	new = malloc(sizeof(list_t));
+	current = *head;
 
-    if (str == NULL || new == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
+	if (str == NULL || new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    new->str = strdup(str);
+	new->str = strdup(str);
 
-    if (new->str == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    new->len = strlen(str);
-    new->next = NULL;
+	new->len = strlen(str);
+	new->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = new;
-    }
-    return (new);
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new;
+	}
+	return (new);
 }
