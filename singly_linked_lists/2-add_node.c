7914ec8bca2184802815944780e363333d2aad4f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-09 18:12:57 +0000

    string.h instead

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -17,7 +17,9 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
     }
 
-    list_t *new = malloc(sizeof(list_t));
+    list_t *new;
+
+    new = malloc(sizeof(list_t));
 
     if (new == NULL)
     {
