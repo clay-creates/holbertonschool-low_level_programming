109408eabe316b5174a90d8fe8589547b78c226a
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-09 18:07:29 +0000

    add stdlib, malloc size of new, stdup str,set head

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -11,8 +12,28 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t new;
+    if (str == NULL)
+    {
+        return (NULL);
+    }
 
-    new = createNode();
-    **head = new;
+    list_t *new = malloc(sizeof(list_t));
+
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+
+    new->str = strdup(str);
+
+    if (new->str == NULL)
+    {
+        return (NULL);
+    }
+
+    new->next = *head;
+
+    *head = new;
+
+    return (new);
 }
