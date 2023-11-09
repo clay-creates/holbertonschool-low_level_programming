af6d6d5fce62cb6a12da2809cc4354b2b0da9dea
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-09 18:58:32 +0000

    make 3-file

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: starting point of linked list
+ * @str: character data in list
+ *
+ * Return: return pointer to new node(s)
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new;
+
+    new = malloc(sizeof(list_t));
+
+    if (str == NULL || new == NULL)
+    {
+        free(new);
+        return (NULL);
+    }
+
+    new->str = strdup(str);
+
+    if (new->str == NULL)
+    {
+        free(new);
+        return (NULL);
+    }
+
+    new->len = strlen(str);
+
+    new->next = new;
+
+    return (new);
+}
