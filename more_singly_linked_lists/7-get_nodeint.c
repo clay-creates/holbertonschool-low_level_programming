29317d865c8ec43b95b540918970fd1e0bd86479
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-10 17:21:26 +0000

    rm index null check

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -21,11 +21,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	}
 
-	if (index == NULL)
-	{
-		return (NULL);
-	}
-
 	for (i = 0; i < index && node_return != NULL; i++)
 	{
 		node_return = node_return->next;
