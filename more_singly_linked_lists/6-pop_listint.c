aa807332202b817bf43a418138f4ea44c00e0639
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-10 16:26:08 +0000

    make 6-file

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t list and returns data
+ * @head: starting point for linked list
+ *
+ * Return: return data from head node (n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (0);
+	}
+
+	if (head != NULL || *head != NULL)
+	{
+		free(*head);
+		return ((*head)->n);
+	}
+}
