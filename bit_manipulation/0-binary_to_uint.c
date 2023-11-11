9023bc828a5ae21d9b432921a186669725c32dc6
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-11 17:56:03 +0000

    push

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,24 +9,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result, i;
+	unsigned int result, i;
 
-    if (b != 0 || b != 1)
-    {
-        return (0);
-    }
+	if (b != 0 || b != 1)
+	{
+		return (0);
+	}
 
-    if (b == NULL)
-    {
-        return (0);
-    }
+	if (b == NULL)
+	{
+		return (0);
+	}
 
-    *b << 1;
+	*b << 1;
 
-    for (i = 0; i != '\0'; i++)
-    {
-        b[i] | result;
-        *b >> 1;
-    }
-    return (result);
+	for (i = 0; i != '\0'; i++)
+	{
+		b[i] | result;
+		*b >> 1;
+	}
+	return (result);
 }
