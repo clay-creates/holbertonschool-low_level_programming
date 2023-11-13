7d9f403c335e68ba98c8e52e8a8eae4377c3daec
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-13 08:17:19 +0000

    make file io files, main.h

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns number of bits needed to flip for one num to another
+ * @n: starting bit
+ * @m: ending bit
+ *
+ * Return: returns number of bits needed on success
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+}
