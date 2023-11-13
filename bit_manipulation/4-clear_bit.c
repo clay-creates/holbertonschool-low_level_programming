445ee468236f3859cc261b4bac39e067d77bbc69
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-13 07:36:22 +0000

    make 4-file

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at given index
+ * @n: bit to set index of
+ * @index: index, starting at 0
+ *
+ * Return: return 1 on success, -1 on fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	*n = *n | (0 << index);
+
+	return (1);
+}
