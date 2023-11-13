6e23ebe75834a48d42fffaebfb4b949c2d9aa495
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-13 19:01:30 +0000

    add more error checks, add write to stdout

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,17 +12,39 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, sz, i;
 	char *c;
+	ssize_t written;
 
-	fd = open(filename, O_RDONLY | O_CREAT);
-	c = (char *)calloc(100, sizeof(char));
-
-	if (fd == -1 || fd == NULL)
+	if (filename == NULL)
 	{
 		return (0);
 	}
 
-	sz = read(fd, c, letters);
-	c[sz] = '\0';
+	fd = open(filename, O_RDONLY | O_CREAT);
 
-	return (0);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	c = (char *)calloc(letters + 1, sizeof(char));
+
+	if (c == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	sz = read(fd, c, letters);
+	if (sz == -1)
+	{
+		close(fd);
+		free(c);
+		return (0);
+	}
+
+	written = write(STDOUT_FILENO, c, sz);
+	close(fd);
+	free(c);
+
+	return (written);
 }
