76a465d1008f4732e7049bd715f4e9559571c3e4
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-13 19:42:54 +0000

    add 0600

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,10 +18,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
+		close(fd);
 		return (-1);
 	}
 
