22b6f6e863e636e28d635522d7cfc87ca53282a3
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-17 14:20:15 +0000

    test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -52,8 +52,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	new->next = current;
-	new->prev = current->prev;
 	if (current->prev != NULL)
 	{
 		current->prev->next = new;
