a141258ae435449b9ef02a784eb169b929fb7719
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-17 15:10:57 +0000

    indentation

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,47 +10,47 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int i;
+	dlistint_t *current;
+	unsigned int i;
 
-    current = *head;
+	current = *head;
 
-    if (*head == NULL)
-    {
-        return (-1);
-    }
+	if (*head == NULL)
+	{
+		return (-1);
+	}
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        if (*head != NULL)
-        {
-            (*head)->prev = NULL;
-        }
-        free(current);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(current);
+		return (1);
+	}
 
-    for (i = 0; i < index && current != NULL; i++)
-    {
-        current = current->next;
-    }
+	for (i = 0; i < index && current != NULL; i++)
+	{
+		current = current->next;
+	}
 
-    if (current == NULL)
-    {
-        return (-1);
-    }
+	if (current == NULL)
+	{
+		return (-1);
+	}
 
-    if (current->prev != NULL)
-    {
-        current->prev->next = current->prev;
-    }
+	if (current->prev != NULL)
+	{
+		current->prev->next = current->prev;
+	}
 
-    if (current->next != NULL)
-    {
-        current->next->prev = current->prev;
-    }
+	if (current->next != NULL)
+	{
+		current->next->prev = current->prev;
+	}
 
-    free(current);
-    return (1);
+	free(current);
+	return (1);
 }
