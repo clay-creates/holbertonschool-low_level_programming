621962db2c1e5ce7e2acb0f47b32b11d18c30019
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-20 22:40:01 +0000

    typo

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -18,14 +18,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
         return (NULL);
     }
 
-    index = key_index(key, ht->size);
+    index = key_index((const char *)key, ht->size);
     current = ht->array[index];
 
     while (current != NULL)
     {
         if (strcmp(current->key, key) == 0)
         {
-            rerutn(current->value);
+            return (current->value);
         }
         current = current->next;
     }
