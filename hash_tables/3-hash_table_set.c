71717e0886d0848bd744608d33881e507b599341
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-20 16:58:26 +0000

    make 3-file

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add to / update key
+ * @key: key
+ * @value: value associated with the key
+ *
+ * Return: return 1 on success, 0 on fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+    hash_node_t *new, *current;
+
+    if (ht == NULL || key == NULL || value == NULL)
+    {
+        return (0);
+    }
+
+    index = key_index((const unsigned char *)key, ht->size);
+
+    current = ht->array[index];
+    while (current != NULL)
+    {
+        if (strcmp(current->key, key) == 0)
+        {
+            free(current->value);
+            current->value = strdup(value);
+            return (1);
+        }
+        current = current->next;
+    }
+
+    new = malloc(sizeof(hash_node_t));
+    if (new == NULL)
+    {
+        return (0);
+    }
+
+    new->key = strdup(key);
+    if (new->key == NULL)
+    {
+        free(new);
+        return (0);
+    }
+}
