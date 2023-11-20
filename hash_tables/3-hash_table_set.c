6fa94b57c77c486e3b8238147a8688c8659db130
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-20 21:42:59 +0000

    indentation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,51 +11,51 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new, *current;
+	unsigned long int index;
+	hash_node_t *new, *current;
 
-    if (ht == NULL || key == NULL || value == NULL)
-    {
-        return (0);
-    }
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
 
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-    current = ht->array[index];
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-        {
-            free(current->value);
-            current->value = strdup(value);
-            return (1);
-        }
-        current = current->next;
-    }
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
 
-    new = malloc(sizeof(hash_node_t));
-    if (new == NULL)
-    {
-        return (0);
-    }
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		return (0);
+	}
 
-    new->key = strdup(key);
-    if (new->key == NULL)
-    {
-        free(new);
-        return (0);
-    }
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
 
-    new->value = strdup(value);
-    if (new->value == NULL)
-    {
-        free(new->key);
-        free(new);
-        return (0);
-    }
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
 
-    new->next = ht->array[index];
-    ht->array[index] = new;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
-    return (1);
+	return (1);
 }
