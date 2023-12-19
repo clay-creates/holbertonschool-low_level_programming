f962ad712c8b106cffe1914346e1b44b2fac6501
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-19 22:03:21 +0000

    update header make 0-file

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for in array
+ *
+ * Return: returns the first index where value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (array[i]);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+}
