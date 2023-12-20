bb4348b2299049a2d7877a30c84fba3044e4806f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-20 17:25:34 +0000

    update again

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -11,24 +11,28 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int low, mid, high;
-
 	if (array == NULL)
 	{
 		return -1;
 	}
 
-	print_array(array, size);
+	printf("Searching in array: ");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
 
-	low = 0;
-	high = size - 1;
+	int low = 0;
+	int high = size - 1;
 
 	while (low <= high)
 	{
-		mid = low + (high - low) / 2;
+		int mid = low + (high - low) / 2;
 
 		if (array[mid] == value)
 		{
+			printf("Found at index: %d\n", mid);
 			return mid;
 		}
 
@@ -41,7 +45,12 @@ int binary_search(int *array, size_t size, int value)
 			high = mid - 1;
 		}
 
-		print_array(array, size);
+		printf("Searching in array: ");
+		for (size_t i = low; i <= high; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
 	}
 
 	return -1;
